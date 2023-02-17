10d98c37a00416a767f8e2e357939d5363cfc8cb
onGod123 <ifejikav4@gmail.com>
2023-02-17 15:41:02 -0800

    positive

--- 0-positive_or_negative.c/0-positive_or_negative.c
+++ 0-positive_or_negative.c/0-positive_or_negative.c
@@ -1,27 +1,29 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 #include <stdio.h>
-/* betty style doc for function main goes there */
 /**
- * main -main function
- *
- * Return: always 0
+ * main - main block
+ * Description: Get a random number and print the number
+ * and if it is positive, negative, or zero
+ * Return: 0
  */
- 
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-        /* your code goes there */
 	if (n > 0)
-		printf("%d is positive", n);
-	else if (n == 0)
-		printf("%d is zero", n);
-        else 
-		 printf("%d is negative", n);
-	printf("\n");
+		printf("%i is positive\n", n);
+	else if (n < 0)
+		printf("%i is negative\n", n);
+	else
+		printf("%i is zero\n", n);
+
 	return (0);
 }
+
+
+
+
+
