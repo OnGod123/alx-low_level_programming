cecf48e972cffa84dce83c82bc12ef59a3f3eb67
onGod123 <ifejikav4@gmail.com>
2023-02-22 07:36:09 -0800

    islower

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,19 +2,23 @@
 /**
  *void print_alphabet_x10 - function that prints 10x alphabets
  *
- *Return: always 0
+ * 2-print_alphabet_x10.c - functions that works
+ *
+ * Return: always 0
  */
 
 
 void print_alphabet_x10(void)
 {
+
 	int i;
 	char j;
 
 	for (i = 1; i <= 10; i++)
 	{
 		for (j = 'a'; j <= 'z'; j++)
-	 		_putchar(j);
+
+			_putchar(j);
 		_putchar('\n');
 	}
 }
