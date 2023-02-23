17ec00af3071423e9c50bb582bdfb0815d9685f2
onGod123 <ifejikav4@gmail.com>
2023-02-22 23:09:03 -0800

    learn

---
+++ 0x02-functions_nested_loops/9-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * times_tables - rows and columns of multiplication
+ * Return: Always 0.
+ */
+int main(void)
+{
+    times_table();
+    return (0);
+}
