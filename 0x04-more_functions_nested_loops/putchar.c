da332c217315e46e1bbad4dff3cd6e780379af29
onGod123 <ifejikav4@gmail.com>
2023-02-24 12:12:52 -0800

    int mul

---
+++ 0x04-more_functions_nested_loops/putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_putchar - for printing strings
+ * return - always zero
+ */
+int main(void)
+{	
+	int _putchar(char c)
+	{	
+		putchar(c);
+		return (0);
+	}
+	
+
+}
