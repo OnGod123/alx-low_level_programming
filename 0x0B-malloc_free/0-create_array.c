a18c6c8585bc565bccc964728bae7958f033452c
onGod123 <ifejikav4@gmail.com>
2023-03-14 02:21:40 -0700

    float

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - Function that creates an array of chars.
+ * @size: Size of the string
+ * @c: The charcter for initialization
+ *
+ * Return: The pointer to the string
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	s = malloc(size * sizeof(char));
+
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		s[i] = c;
+
+	}
+	s[i] = '\0';
+	return (s);
+}
