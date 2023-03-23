8f99a8a4cf941dbd347a303ed14dfe258626bdc1
onGod123 <ifejikav4@gmail.com>
2023-03-22 19:23:21 -0700

    To hell with circumstances; I create opportunities

---
+++ mai.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+
+
+int main(void){
+	int sum = 0;
+	print_hello_world();
+	sum = add_number(10,33);
+	printf("%d\n", sum);
+	
+}
---
+++ main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN_H
+#define MAN_H
+void print_hello_world(void);
+int add_number(int a, int b);
+#endif
