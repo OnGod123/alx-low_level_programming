b76c50965488e4f68c5ae53d53b47b0fd1b112f6
onGod123 <ifejikav4@gmail.com>
2023-03-30 15:00:06 -0700

    free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,25 +1,19 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * free_list - frees a list
+ * @head: head of the linked list.
+ * Return: no return.
  */
-int main(void)
-{
-	list_t *head;
 
-	head = NULL;
-	add_node_end(&head, "Bob");
-	add_node_end(&head, "&");
-	add_node_end(&head, "Kris");
-	add_node_end(&head, "love");
-	add_node_end(&head, "asm");
-	print_list(head);
-	free_list(head);
-	head = NULL;
-	return (0);
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while ((current = head) != NULL)
+	{
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
 }
