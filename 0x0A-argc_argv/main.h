c4172f27eb768bbe0487934d863a580c797c60c8
onGod123 <ifejikav4@gmail.com>
2023-06-15 13:28:24 -0700

    argv task

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+char _putchar(int c);
+
+#endif
