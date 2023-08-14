bd763c72f0a590cb25f1488787ae9d1d08812ed4
root <ifejikav4@gmail.com>
2023-08-14 05:25:43 -0700

    we kept believing

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints current the filename to std output
+ *
+ * Return: 0 to show no error
+ */
+#define _FILE_  "2-main.c"
+int main(void)
+{
+	printf("%s\n", _FILE_);
+	return (0);
+}
