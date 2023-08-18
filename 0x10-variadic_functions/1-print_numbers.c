2ed1095470a3014d6ea49a7a7de341810d725214
root <ifejikav4@gmail.com>
2023-08-18 06:42:09 -0700

    it is scary when you have failed alot

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,9 +20,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 va_list kwargs;
 
+unsigned int i;
+
 va_start(kwargs, n);
 
-for (unsigned int i = 0; i < n; i++)
+for (i = 0; i < n; i++)
 {
 int decimal = va_arg(kwargs, int);
 printf("%d", decimal);
