fe6d909d7ae457bdf66b8bc8e97f05d0b1491e13
OnGod123 <ifejikav4@gmail.com>
2023-08-29 00:08:43 -0700

    if being discipline was easy

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -5,8 +5,8 @@
 #include <stdlib.h>
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 size_t print_listint(const listint_t *h);
 #endif
